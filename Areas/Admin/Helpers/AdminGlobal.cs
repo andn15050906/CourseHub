c1023e9797dfc653fd5f22dfed6b923ff9859ec1
DAO LE HUY <128690957+OkamiSeguri@users.noreply.github.com>
2023-10-13 16:01:56 +0700

    Add files via upload

---
+++ Areas/Admin/Helpers/AdminGlobal.cs
@@ -0,0 +1,15 @@
+﻿namespace CourseHub.UI.Areas.Admin.Helpers;
+
+public class AdminGlobal
+{
+    public const string TITLE = "Title";
+
+    public const string PAGE_INDEX = "/Index";
+    public const string PAGE_USERS_INDEX = "/Users/Index";
+    public const string PAGE_NOTIFICATIONS_INDEX = "/Notifications/Index";
+    public const string PAGE_BLOCK_USERS_ACCOUNT_INDEX = "/BlockUsersAccount/Index";
+    public const string PAGE_CREATE_ADMIN_INDEX = "/CreateAdmin/Index";
+    public const string PAGE_REPORTED_GROUP_INDEX = "/ReportedGroupManagement/Index";
+
+    public const string LAYOUT_BASE = "_BaseLayout";
+}
